a74d8458ac20b3213eea5d260c137ad5ccf8a0ce
ASimler <adelesimler96@gmail.com>
2025-04-30 20:16:45 +0200

    avancement cours 1 react

---
+++ cours-react-1/src/components/Countries.js
@@ -0,0 +1,19 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+
+const Countries = () => {
+  // Le useEffect se joue lorsque le composant est monté 'créé'
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => console.log(res));
+  }, []);
+
+  return (
+    <div className="countries">
+      <h1>COUNTRIES</h1>
+    </div>
+  );
+};
+
+export default Countries;
---
+++ cours-react-1/src/pages/About.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Logo from "../components/Logo";
+import Navigation from "../components/Navigation";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A PROPOS</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat atque
+        facilis error voluptatibus quam dolores doloremque odit suscipit quasi
+        corrupti iusto officiis maxime, maiores harum quo beatae dolor eos
+        consequuntur sint pariatur exercitationem, officia, repudiandae neque.
+        Inventore, quos odio? Eaque ad maxime expedita! Possimus cum eius,
+        aliquam itaque odit praesentium quia suscipit officiis ullam doloribus
+        repellat reprehenderit error, quos optio dolores minima, obcaecati
+        dignissimos asperiores rerum sed consequatur corrupti minus dolorem.
+        Possimus ipsum doloremque fugiat at saepe, explicabo rerum incidunt
+        ipsam officia quas libero sequi adipisci maiores deleniti praesentium
+        reiciendis harum assumenda vitae nulla natus in autem quasi corporis
+        dolore.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quisquam,
+        voluptatem repellendus assumenda sed nisi ad dolore iusto quae corporis
+        ipsum omnis, nostrum ab reiciendis esse minus animi, maiores natus
+        laborum aspernatur nihil. Blanditiis repellendus incidunt quibusdam,
+        maiores debitis doloremque. Modi distinctio iure eius quaerat aut eos
+        voluptatum eaque voluptates at itaque qui quod animi reiciendis veniam
+        possimus, laudantium odit expedita obcaecati alias nisi perferendis
+        pariatur debitis aspernatur deserunt! Cum ratione quidem fuga officiis
+        eius rerum modi tempora corporis consequatur aut quam esse porro enim
+        voluptate necessitatibus expedita nisi, dolores, dolorem asperiores
+        maxime beatae incidunt. Aspernatur suscipit blanditiis cumque obcaecati
+        ab!
+      </p>
+    </div>
+  );
+};
+
+export default About;
