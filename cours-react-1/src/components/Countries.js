e4769e7371b11eaaf24782fd4f6ce6bf4fddab63
ASimler <adelesimler96@gmail.com>
2025-05-12 15:03:00 +0200

    suite cours react

--- cours-react-1/src/components/Countries.js
+++ cours-react-1/src/components/Countries.js
@@ -2,7 +2,7 @@ import React, { useEffect } from "react";
 import axios from "axios";
 
 const Countries = () => {
-  // Le useEffect se joue lorsque le composant est monté 'créé'
+  // Le useEffect se joue lorsque le composant est monté c'est à dire créé
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
