691576d3c70d09630462dc4c154f052d874e4b83
ASimler <adelesimler96@gmail.com>
2025-05-13 17:46:36 +0200

    first course done

--- cours-react-1/src/components/Countries.js
+++ cours-react-1/src/components/Countries.js
@@ -7,6 +7,8 @@ import Card from "./Card";
 const Countries = () => {
   const [data, setData] = useState([]);
   const [rangeValue, setRangeValue] = useState(36);
+  const [selectedRadio, setSelectedRadio] = useState("");
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
 
   // Le useEffect se joue lorsque le composant est monté c'est à dire créé
   useEffect(() => {
@@ -29,11 +31,36 @@ const Countries = () => {
             setRangeValue(e.target.value);
           }}
         />
-      </ul>
-      <ul>
-        {data.slice(0, rangeValue).map((country, index) => (
-          <Card key={index} country={country} />
+        {/* Pour éviter de répéter 5 fois l'input : <input type="radio" id="afrique" />
+        <label htmlFor="afrique"></label> 
+        => */}
+        {radios.map((continent) => (
+          <li>
+            <input
+              type="radio"
+              id={continent}
+              name="contientRadio"
+              checked={continent === selectedRadio}
+              onChange={(e) => setSelectedRadio(e.target.id)}
+            />
+            <label htmlFor={continent}>{continent}</label>
+          </li>
         ))}
+        {/* le name du radio permet de les lier et de ne pas pouvoir tous les cocher */}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
+      <ul>
+        {data
+          .filter((country) => country.continents[0].includes(selectedRadio))
+          .sort((a, b) => b.population - a.population)
+          .slice(0, rangeValue)
+          .map((country, index) => (
+            <Card key={index} country={country} />
+          ))}
       </ul>
       {/* On crée un composant enfant de countries qui sera "card". Or card ne connait pas les données des pays, ils faut donc les lui transmettre en passant le country en paramètre lors du motage du composant et en destructurant country dans le paramètre du composant Card.js */}
     </div>
