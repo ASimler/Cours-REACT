17b0ffd91278907897f57b0689b8f6a4f788bb7d
ASimler <adelesimler96@gmail.com>
2025-05-13 22:37:21 +0200

    react course v2

--- cours-react-1/src/App.js
+++ cours-react-1/src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Blog from "./pages/Blog";
 
 const App = () => {
   return (
@@ -10,6 +11,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/blog" element={<Blog />} />
 
         {/* path="*" fonctionne si jamais l'url ne correspond à rien de déclaré au dessus */}
         <Route path="*" element={<Home />} />
